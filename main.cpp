78092e2a19a06c53bf7452097c5d71a726bfe0e4
perinp_r <perinp_r@epitech.eu>
2015-05-19 13:33:02 +0200

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//
+// main.cpp for main in /home/perinp_r/Bitbucket/bomberman
+// 
+// Made by ravivarman perinpanathan
+// Login   <perinp_r@epitech.net>
+// 
+// Started on  Tue May 19 12:49:55 2015 ravivarman perinpanathan
+// Last update Tue May 19 12:54:01 2015 ravivarman perinpanathan
+//
+
+#include <cstdlib>
+#include "GameEngine.hpp"
+int	main()
+{
+  //On crees on engine GameEngine  engine;
+  if (engine.initialize() == false)
+    return (EXIT_FAILURE);
+  while (engine.update() == true)
+    engine.draw();
+  return
+    EXIT_SUCCESS;
+}
