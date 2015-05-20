0b3c0c690438b83a3df30118d92dda8291f40103
tran_1 <tran_1@epitech.eu>
2015-05-20 11:45:05 +0200

    Delete IronManBug branch

--- AObject.hh
+++ AObject.hh
@@ -22,8 +22,7 @@ public:
   virtual bool initialize();
 
   // La fonction update sert a gerer le comportement de l'objet
-  virtual void update(gdl::Clock const &clock, gdl::Input &input)
-  {}
+  virtual void update(gdl::Clock const &clock, gdl::Input &input);
 
   // La fonction draw sert a dessiner l'objet
   virtual void draw(gdl::AShader &shader, gdl::Clock const &clock) = 0;
