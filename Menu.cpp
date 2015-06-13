ab6f9388d1c7392a3b271857d83fddef85960d3a
navarr_a <navarr_a@epitech.eu>
2015-06-13 17:28:41 +0200

    Add old files

---
+++ Menu.cpp
@@ -0,0 +1,45 @@
+#include "Menu.hh"
+
+Menu::Menu()
+{
+}
+
+Menu::~Menu()
+{
+}
+
+bool	Menu::initialize()
+{
+  _speed = 10.0f;
+  if (_texture.load("./") == false)
+    {
+      std::cerr << " Couldnt load the Menu  image" << std::endl;
+      return (false);
+    }
+
+  _geometry.pushVertex(glm::vec3(0.5, -0.5, 0.5));
+  _geometry.pushVertex(glm::vec3(0.5, 0.5, 0.5));
+  _geometry.pushVertex(glm::vec3(-0.5, 0.5, 0.5));
+  _geometry.pushVertex(glm::vec3(-0.5, -0.5, 0.5));
+  _geometry.pushUv(glm::vec2(0.0f, 0.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 0.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 1.0f));
+  _geometry.pushUv(glm::vec2(0.0f, 1.0f));
+  _geometry.build();
+  return (true);
+}
+
+void	Menu::update(gdl::Clock const &clock, gdl::Input &input)
+{
+  if (input.getKey(SDLK_UP))
+    translate(glm::vec3(0, 0, -1) * static_cast<float>(clock.getElapsed()) * _speed);
+  if (input.getKey(SDLK_DOWN))
+    translate(glm::vec3(0, 0, 1) * static_cast<float>(clock.getElapsed()) * _speed);
+}
+
+void	Menu::draw(gdl::AShader &shader, gdl::Clock const &clock)
+{
+  (void)clock;
+  _texture.bind();
+  _geometry.draw(shader, getTransformation(), GL_QUADS);
+}
